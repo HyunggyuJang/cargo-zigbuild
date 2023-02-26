b05d681fa3e57ea98199b52d0b038bc9426d3934
Hyunggyu Jang <murasakipurplez5@gmail.com>
2023-02-26 13:57:07 +0900

    Fix

--- src/zig.rs
+++ src/zig.rs
@@ -211,17 +211,10 @@ impl Zig {
             if let Some(sdkroot) = Self::macos_sdk_root() {
                 let sdkroot = Path::new(&sdkroot);
                 new_cmd_args.extend_from_slice(&[
-                    format!("-I{}", sdkroot.join("usr").join("include").display()),
-                    format!("-L{}", sdkroot.join("usr").join("lib").display()),
-                    format!(
-                        "-F{}",
-                        sdkroot
-                            .join("System")
-                            .join("Library")
-                            .join("Frameworks")
-                            .display()
-                    ),
-                    "-DTARGET_OS_IPHONE=0".to_string(),
+                    format!("--sysroot={}", sdkroot.display()),
+                    "-I/usr/include".to_string(),
+                    "-L/usr/lib".to_string(),
+                    "-F/System/Library/Frameworks".to_string(),
                 ]);
             }
         }
